b8cf3513887b020c61231e8ee5f439af59922a08
Johaen11 <johngnanakumar@gmail.com>
2016-05-13 16:40:19 -0400

    Added GetUserName method

--- COMP123-S2016-Lesson1B/Program.cs
+++ COMP123-S2016-Lesson1B/Program.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Author: Johaen Gnanakumar
+ * Date Created: May 13,2016
+ * Date Modified: May 13, 2016
+ * Student ID: 300880309
+ * 
+ * 
+ *Description: This Program demonstrates GitHub and version control 
+ * best practices. We Also explore Unit Testing
+ * 
+ * Version: 0.2 - Added GetUserName method
+ */
+
+
+
 namespace COMP123_S2016_Lesson1B
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GetUserName();
+        }
+        
+        
+     /*This method reads the UserName from the Console
+      * 
+      * @method GetUserName
+      * @return {string} UserName
+      * 
+      */
+
+        public static string GetUserName()
+        { 
+            //Initialize Variable
+            String UserName = "";
+            Console.Write("Enter UserName: ");
+           UserName =  Console.ReadLine();
+            Console.WriteLine("/n=====================");
+            Console.WriteLine("You Entered: "+ UserName);
+
+            return UserName;
         }
     }
 }
